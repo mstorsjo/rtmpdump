02272037f2c518e462d1a2c38766aba4ac4e5294
hyc <hyc@400ebc74-4327-4243-bc38-086b20814532>
2010-03-12 23:53:41 +0000

    Tweak prev commit
    git-svn-id: svn://svn.mplayerhq.hu/rtmpdump/trunk@335 400ebc74-4327-4243-bc38-086b20814532

--- librtmp/rtmp.h
+++ librtmp/rtmp.h
@@ -45,9 +45,8 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #define GetSockError()	errno
-#ifndef closesocket
+#undef closesocket
 #define closesocket(s)	close(s)
-#endif
 #define msleep(n)	usleep(n*1000)
 #define SET_RCVTIMEO(tv,s)	struct timeval tv = {s,0}
 #endif

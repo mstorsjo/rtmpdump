8d9e9649ab51cc36b31c48545e5fe3783003cff4
hyc <hyc@400ebc74-4327-4243-bc38-086b20814532>
2010-06-03 16:50:30 +0000

    Update FSF address in license notice
    git-svn-id: svn://svn.mplayerhq.hu/rtmpdump/trunk@505 400ebc74-4327-4243-bc38-086b20814532

--- librtmp/handshake.h
+++ librtmp/handshake.h
@@ -17,7 +17,8 @@
  *
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with librtmp see the file COPYING.  If not, write to
- *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA  02110-1301, USA.
  *  http://www.gnu.org/copyleft/lgpl.html
  */
 
